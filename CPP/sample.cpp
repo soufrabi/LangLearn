9a0693ecc550e1b2f78eff7fb406ce56c38e85f7
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-07-29 12:02:46 +0530

    Remove files without extension

---
+++ CPP/sample.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int li;
+typedef long double ld;
+
+
+int f(){
+    
+
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int tt;
+    cin >> tt;
+
+    while (tt-- > 0)
+    {
+        
+    }
+
+    return 0;
+}
